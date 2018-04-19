191cad8dddfa0f8e8f92c94e0137c6c5416e2a8a
Jayprakash12345 <0freerunning@gmail.com>
2018-04-19 17:15:23 +0000

    Remove 'UnitTestList' hook
    Bug: T142120
    Change-Id: Icf6bd324972253ee15813e6bc67f9ea05c2913f3

--- PhpTagsFunctions.hooks.php
+++ PhpTagsFunctions.hooks.php
@@ -45,15 +45,4 @@ class PhpTagsFunctionsHooks {
 		return true;
 	}
 
-	/**
-	 *
-	 * @param array $files
-	 * @return bool
-	 */
-	public static function onUnitTestsList( &$files ) {
-		$testDir = __DIR__ . '/tests/phpunit';
-		$files = array_merge( $files, glob( "$testDir/*Test.php" ) );
-		return true;
-	}
-
 }
