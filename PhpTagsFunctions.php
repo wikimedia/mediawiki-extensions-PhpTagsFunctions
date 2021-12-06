da43ac88120e348cf4d8ffa19f7fd03083b814e6
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 17:36:47 +0100

    Emit deprecation warning from php entry point
    Change-Id: Ib35d7a5c91e2fbcf4e1c969ce358d4b44ed603e7

--- PhpTagsFunctions.php
+++ PhpTagsFunctions.php
@@ -3,12 +3,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'PhpTagsFunctions' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['PhpTagsFunctions'] = __DIR__ . '/i18n';
-// wfWarn(
-// 'Deprecated PHP entry point used for PhpTags Functions extension. ' .
-// 'Please use wfLoadExtension instead, ' .
-// 'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-// );
+	wfWarn(
+		'Deprecated PHP entry point used for PhpTags Functions extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return;
 } else {
-	die( 'This version of the PhpTags Functions extension requires MediaWiki 1.25+' );
+	die( 'This version of the PhpTags Functions extension requires MediaWiki 1.34+' );
 }
