96cb774a6bc05026d47456f715112a5897ebe732
Umherirrender <umherirrender_de.wp@web.de>
2023-05-06 16:39:56 +0200

    Replace deprecated Hooks::run
    Bug: T335536
    Change-Id: I37b95936694af0d5efd965d2ea28fe55025ed819

--- tests/phpunit/!!!firstInit_Test.php
+++ tests/phpunit/!!!firstInit_Test.php
@@ -2,7 +2,7 @@
 
 if ( PhpTags\Renderer::$needInitRuntime ) {
 	wfDebug( 'PHPTags: run hook PhpTagsRuntimeFirstInit ' . __FILE__ );
-	\Hooks::run( 'PhpTagsRuntimeFirstInit' );
+	\MediaWiki\MediaWikiServices::getInstance()->getHookContainer()->run( 'PhpTagsRuntimeFirstInit' );
 	\PhpTags\Hooks::loadData();
 	\PhpTags\Runtime::$loopsLimit = 1000;
 	PhpTags\Renderer::$needInitRuntime = false;
